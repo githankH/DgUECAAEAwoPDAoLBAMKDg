0266bf4f453a5296f44f34760da134fd998e54e4
Hank (Yuan-Lung) Hsiao <ylhsiao@ambarella.com>
2018-11-30 10:11:36 +0800

    add build folder again

---
+++ build/precache-manifest.0682fd024a113f0810152bdb1dc4de20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "475cfcd16b044a46a1d0",
+    "url": "./static/js/main.475cfcd1.chunk.js"
+  },
+  {
+    "revision": "b6f27de7ad34621a8d1c",
+    "url": "./static/js/1.b6f27de7.chunk.js"
+  },
+  {
+    "revision": "475cfcd16b044a46a1d0",
+    "url": "./static/css/main.8da9e0bd.chunk.css"
+  },
+  {
+    "revision": "b6f27de7ad34621a8d1c",
+    "url": "./static/css/1.08133bf5.chunk.css"
+  },
+  {
+    "revision": "1c983b928a79a7ad36c5d06647ebde94",
+    "url": "./index.html"
+  }
+];
