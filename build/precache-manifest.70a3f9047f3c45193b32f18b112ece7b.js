d750a92a1367c163b70ff7d7283ea061f71262fb
hank.h <villiard+githank@gmail.com>
2018-11-30 08:12:07 +0800

    Add demo result

---
+++ build/precache-manifest.70a3f9047f3c45193b32f18b112ece7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "67911bb3685d574bdd71",
+    "url": "/static/js/main.67911bb3.chunk.js"
+  },
+  {
+    "revision": "6c1f4664f914d50c2cf4",
+    "url": "/static/js/1.6c1f4664.chunk.js"
+  },
+  {
+    "revision": "67911bb3685d574bdd71",
+    "url": "/static/css/main.b5938734.chunk.css"
+  },
+  {
+    "revision": "6c1f4664f914d50c2cf4",
+    "url": "/static/css/1.2f5dfb24.chunk.css"
+  },
+  {
+    "revision": "d67cfaedc57ba8edc5ed8bb6bb037b0f",
+    "url": "/index.html"
+  }
+];
